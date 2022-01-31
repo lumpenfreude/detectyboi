5d15a4a37a034c755a3e064ddbcfc221125b2e7b
lumpenfreude <lumpenfreude@gmail.com>
2022-01-31 11:25:37 -0600

    lol complete rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,60 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+#include <MQ2.h>
+#include "MQ7.h"
+#include <GP2YDustSensor.h> 
 
-#define MQ7_PINA 34
-#define MQ7_PIND 27
-#define MQ7_ELED 0
-#define MQ2_PINA 35
-#define MQ2_PIND 32
-#define MQ2_ELED 2
-int MQ2limit;
-int MQ2value;
-int MQ7limit;
-int MQ7value;
+#define LEDPIN 2
+#define BEEPER 0
+int mq2_analog = 33;
+int mq7_analog = 34;
+const uint8_t SHARP_LED_PIN = 4;
+const uint8_t SHARP_VO_PIN = 35;
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
+MQ2 mq2(mq2_analog);
+MQ7 mq7(mq7_analog,5.0);
+GP2YDustSensor dustSensor(GP2YDustSensorType::GP2Y1010AU0F, SHARP_LED_PIN, SHARP_VO_PIN);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(MQ7_PINA, INPUT);
-  pinMode(MQ7_PIND, INPUT);
-  pinMode(MQ7_ELED, OUTPUT);
-  pinMode(MQ7_PINA, INPUT);
-  pinMode(MQ7_PIND, INPUT);
-  pinMode(MQ7_ELED, OUTPUT);
+  dustSensor.begin();
+  mq2.begin();
+  bmp.begin_I2C();
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
 void loop() {
-  MQ2limit = digitalRead(MQ2_PIND);
-  MQ2value = analogRead(MQ2_PINA);
-  MQ7limit = digitalRead(MQ7_PIND);
-  MQ7value = analogRead(MQ7_PINA);
-  Serial.print("MQ2 Value: ");
-  Serial.println(MQ2limit, MQ2value);
-  delay(250);
-  Serial.print("MQ7 Value: ");
-  Serial.println(MQ7limit, MQ7value);
-  delay(250);
+  Serial.print("Dust density: ");
+  Serial.print(dustSensor.getDustDensity());
+  Serial.println(" ppm");
+  delay(500);
+  Serial.print("Carbon Monoxide: ");
+  Serial.print(mq7.getPPM());
+  Serial.println(" ppm");
+  delay(500);
+  Serial.print("LPG: ");
+  Serial.print(mq2.readLPG());
+  Serial.println(" ppm");
+  delay(500);
+  Serial.print("Smoke: ");
+  Serial.print(mq2.readSmoke());
+  Serial.println(" ppm");
+  delay(500);
+  Serial.print("Temperature: ");
+  Serial.print(bmp.temperature);
+  Serial.println("°C");
+  delay(500);
+  Serial.print("Barometric Pressure: ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+  delay(500);
 }
